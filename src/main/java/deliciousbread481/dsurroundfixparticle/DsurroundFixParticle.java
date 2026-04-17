8bffa36ff349d03d784bad9cedbc07766aaa91f8
DeliciousBread481 <3574467868@qq.com>
2026-04-17 21:34:30 +0800

    fix class name

--- src/main/java/deliciousbread481/dsurroundfixparticle/DsurroundFixParticle.java
+++ src/main/java/deliciousbread481/dsurroundfixparticle/DsurroundFixParticle.java
@@ -4,12 +4,12 @@ import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;  
 import org.slf4j.LoggerFactory;  
   
-@Mod(DsurroundFix.MOD_ID)  
+@Mod(DsurroundFixParticle.MOD_ID)  
 public class DsurroundFixParticle {  
     public static final String MOD_ID = "dsurroundfixparticle";  
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);  
   
-    public DsurroundFix() {  
+    public DsurroundFixParticle() {  
         LOGGER.info("DsurroundFixParticle loaded - ObjectArray.forEach null safety patch active");  
     }  
 }
